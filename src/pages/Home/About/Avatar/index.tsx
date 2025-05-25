89e31dd8ee468f2f081948f5cc2e874138c283a1
CK <2655257336@qq.com>
2025-05-25 21:06:46 +0800

    图片

--- src/pages/Home/About/Avatar/index.tsx
+++ src/pages/Home/About/Avatar/index.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import "./index.scss";
 
 const Avatar = () => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <div className="avatar">
-      <img src="/avatar1.png" alt="" />
+    <div
+      className={`avatar ${isHovered ? "hovered" : ""}`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div className="tech-icons">
+        <img className="icon vscode" src="/vscode.svg" alt="VSCode" />
+        <img className="icon github" src="/github.svg" alt="GitHub" />
+        <img className="icon vue" src="/vue.svg" alt="Vue" />
+        <img className="icon react" src="/react.svg" alt="React" />
+      </div>
+      <img className="avatar-img" src="/avatar1.png" alt="" />
     </div>
   );
 };
